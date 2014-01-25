ef1fce5ee3792f395dab7067507a3dc53ff15960
anvaka <anvaka@gmail.com>
2014-01-25 01:08:09 -0800

    Added basic parsing support

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ module.exports.load = load;
 
 function save() {}
 
-function load() {}
+function load(gexfContent, completeCb) {
+  var parser = require('./lib/gexfParser')(gexfContent);
+  var graph = require('ngraph.graph')();
+  graph.beginUpdate();
+
+  parser.on('node', function (id, data) {
+    graph.addNode(id, data);
+  }).on('link', function (id, from, to) {
+    var link = graph.addLink(from, to);
+    link.id = id; // gexf has own link identifiers, use that;
+  }).on('end', function (err) {
+    graph.endUpdate();
+    completeCb(err, graph);
+  });
+}
--- test/read.js
+++ test/read.js
@@ -1,6 +1,25 @@
 var test = require('tap').test,
-    gexf = require('../');
+    gexf = require('../'),
+    fs = require('fs');
 
 test('Can read data', function (t) {
+  gexf.load(fs.readFileSync(__dirname + '/data/basic.gexf', 'utf8'), function (err, graph) {
+    t.ok(err === undefined, 'No error');
+    t.equal(graph.getNodesCount(), 2, 'has two nodes');
+    t.equal(graph.getLinksCount(), 1, 'has one link');
 
+    var hello = graph.getNode('0');
+    var world = graph.getNode('1');
+
+    t.equal(hello.data.label, 'Hello', 'Reads label');
+    t.equal(world.data.label, 'World', 'Reads label');
+
+    graph.forEachLink(function (link) {
+      t.equal(link.fromId, '0');
+      t.equal(link.toId, '1');
+      t.equal(link.id, '0');
+    });
+
+    t.end();
+  });
 });
