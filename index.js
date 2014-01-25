284fd846201fbdb66f030aa96bf037c46ebca617
anvaka <anvaka@gmail.com>
2014-01-25 05:09:38 -0800

    Using xpath instead

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 module.exports.save = save;
-module.exports.load = load;
+module.exports.load = require('./lib/load');
 
 
 function save() {}
 
-function load(gexfContent, completeCb) {
-  var parser = require('./lib/gexfParser')(gexfContent);
-  var graph = require('ngraph.graph')();
-  graph.beginUpdate();
-
-  parser.on('node', function (id, data) {
-    graph.addNode(id, data);
-  }).on('link', function (id, from, to) {
-    var link = graph.addLink(from, to);
-    link.id = id; // gexf has own link identifiers, use that;
-  }).on('end', function (err) {
-    graph.endUpdate();
-    completeCb(err, graph);
-  });
-}
--- test/read.js
+++ test/read.js
@@ -2,24 +2,49 @@ var test = require('tap').test,
     gexf = require('../'),
     fs = require('fs');
 
-test('Can read data', function (t) {
-  gexf.load(fs.readFileSync(__dirname + '/data/basic.gexf', 'utf8'), function (err, graph) {
-    t.ok(err === undefined, 'No error');
-    t.equal(graph.getNodesCount(), 2, 'has two nodes');
-    t.equal(graph.getLinksCount(), 1, 'has one link');
+test('Can read basic', function (t) {
+  var graph = gexf.load(fs.readFileSync(__dirname + '/data/basic.gexf', 'utf8'));
 
-    var hello = graph.getNode('0');
-    var world = graph.getNode('1');
+  t.equal(graph.getNodesCount(), 2, 'has two nodes');
+  t.equal(graph.getLinksCount(), 1, 'has one link');
 
-    t.equal(hello.data.label, 'Hello', 'Reads label');
-    t.equal(world.data.label, 'World', 'Reads label');
+  var hello = graph.getNode('0');
+  var world = graph.getNode('1');
 
-    graph.forEachLink(function (link) {
-      t.equal(link.fromId, '0');
-      t.equal(link.toId, '1');
-      t.equal(link.id, '0');
-    });
+  t.equal(hello.data.label, 'Hello', 'Reads label');
+  t.equal(world.data.label, 'World', 'Reads label');
 
-    t.end();
+  graph.forEachLink(function (link) {
+    t.equal(link.fromId, '0');
+    t.equal(link.toId, '1');
+    t.equal(link.id, '0');
   });
+
+  t.end();
+});
+
+test('Can read data', function (t) {
+  var graph = gexf.load(fs.readFileSync(__dirname + '/data/data.gexf', 'utf8'));
+  t.equal(graph.getNodesCount(), 4, 'has expected number of nodes');
+  t.equal(graph.getLinksCount(), 5, 'has expected number of links');
+
+  var barabasiLab = graph.getNode('3').data;
+
+  t.equal(barabasiLab.label, 'BarabasiLab', 'Reads label');
+  t.equal(barabasiLab.url, 'http://barabasilab.com', 'Reads data');
+
+  /*
+  t.equal(barabasiLab.indegree, 1, 'Reads typed data');
+  t.equal(barabasiLab.frog, false, 'Reads typed data');
+
+  var gephi = graph.getNode('0').data;
+  t.equal(gephi.from, true, 'Respects defaults');
+  graph.forEachLink(function (link) {
+    t.equal(link.fromId, '0');
+    t.equal(link.toId, '1');
+    t.equal(link.id, '0');
+  });
+  */
+
+  t.end();
 });
