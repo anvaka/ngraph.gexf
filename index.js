c8397e82940b1033ffaa15a9907591156298c32c
anvaka <anvaka@gmail.com>
2014-01-25 00:00:41 -0800

    Added basic scaffolding

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports.save = save;
+module.exports.load = load;
+
+
+function save() {}
+
+function load() {}
---
+++ test/read.js
@@ -0,0 +1,6 @@
+var test = require('tap').test,
+    gexf = require('../');
+
+test('Can read data', function (t) {
+
+});
